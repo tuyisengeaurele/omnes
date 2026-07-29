8e62846ddc67ec81cec3f3eb05728c6f4dfedc65
Ange Aurele TUYISENGE <tuyisengeauris@gmail.com>
2026-07-29 16:05:02 +0200

    Wire authentication IPC channels

--- electron/preload/index.ts
+++ electron/preload/index.ts
@@ -4,6 +4,19 @@ import { IPC_CHANNELS, type AppApi } from '@shared/ipc';
 const api: AppApi = {
   getAppVersion: () => ipcRenderer.invoke(IPC_CHANNELS.getAppVersion),
   checkDatabaseHealth: () => ipcRenderer.invoke(IPC_CHANNELS.checkDatabaseHealth),
+  hasUsers: () => ipcRenderer.invoke(IPC_CHANNELS.hasUsers),
+  createFirstAdmin: (username, password) =>
+    ipcRenderer.invoke(IPC_CHANNELS.createFirstAdmin, username, password),
+  login: (username, password) => ipcRenderer.invoke(IPC_CHANNELS.login, username, password),
+  logout: () => ipcRenderer.invoke(IPC_CHANNELS.logout),
+  unlock: (password) => ipcRenderer.invoke(IPC_CHANNELS.unlock, password),
+  getSession: () => ipcRenderer.invoke(IPC_CHANNELS.getSession),
+  getLastUsername: () => ipcRenderer.invoke(IPC_CHANNELS.getLastUsername),
+  onSessionLocked: (callback) => {
+    const listener = () => callback();
+    ipcRenderer.on(IPC_CHANNELS.sessionLocked, listener);
+    return () => ipcRenderer.removeListener(IPC_CHANNELS.sessionLocked, listener);
+  },
 };
 
 contextBridge.exposeInMainWorld('omnes', api);
